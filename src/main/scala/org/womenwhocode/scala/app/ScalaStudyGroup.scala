1d7ec2f3709f59734c67bbf8f2b07318f6eab4d3
Kelsey <kelseyq@gmail.com>
2013-04-11 18:30:47 -0700

    front page

--- src/main/scala/org/womenwhocode/scala/app/ScalaStudyGroup.scala
+++ src/main/scala/org/womenwhocode/scala/app/ScalaStudyGroup.scala
@@ -1,15 +1,21 @@
 package org.womenwhocode.scala.app
 
 import org.scalatra._
-import scalate.ScalateSupport
+import java.util.Calendar
 
 class ScalaStudyGroup extends ScalaStudyGroupStack {
 
   get("/") {
     <html>
       <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+        <center>
+        	<h1>Hello, world!</h1>
+        	<center><img src="http://www.foodtruckcourt.com/img/fewdtruck.png"/></center>
+        	<hr/>
+      		<p>
+       			It's {Calendar.getInstance().getTime()} and the Women Who Code Scala study group still rocks!
+      		 </p>
+       </center>
       </body>
     </html>
   }  
