b48793a2ee5a1b62c589072476f78a7be91587af
Kelsey <kelseyq@gmail.com>
2013-04-15 08:41:21 -0700

    examples from 4/11 group

--- src/main/scala/org/womenwhocode/scala/app/ScalaStudyGroup.scala
+++ src/main/scala/org/womenwhocode/scala/app/ScalaStudyGroup.scala
@@ -14,9 +14,9 @@ class ScalaStudyGroup extends ScalaStudyGroupStack {
         	<hr/>
       		<p>
        			It's {Calendar.getInstance().getTime()} and the Women Who Code Scala study group still rocks!
-      		 </p>
+      		</p>
       		<p>
-      			<a href="/trucks">Food Truck List</a>
+      			<a href="/trucks">Food Truck List!!!! YEA!</a>
       		</p>
        </center>
       </body>
@@ -26,7 +26,40 @@ class ScalaStudyGroup extends ScalaStudyGroupStack {
   get("/trucks") {
   	<html>
   		<body>
-  		TODO
+  		<h2>The first truck is...</h2>
+  		{
+  		    val theTruck = FoodTruckData.getAllFoodTrucks(0) 
+  			//pattern matching on a case class
+  		    theTruck.cuisineType match {
+  		    	case Tacos => <p>Mexican!</p>
+  		    	case Indian => <p>Indian!</p>
+  		    	case _ => <p>Something else!</p>
+  		    }
+  		}
+  		<h2>Is it tacos?</h2>
+  		<ul>
+  		{
+
+  			//map and an anonymous function
+  	      FoodTruckData.getAllFoodTrucks.map{
+  	      		//using pattern matching to extract members
+  	      		theTruck: FoodTruck => theTruck match {
+  	      			case FoodTruck(theName, Tacos, _, _) => <li>{theName}! Eff yeah!</li>
+  	      			case _ => <li>{theTruck.name} (not tacos)</li>
+  	      		}
+  	      	}
+		}
+		</ul>
+		<h2>Just one truck</h2>
+		{
+			val myList = FoodTruckData.getAllFoodTrucks
+			//pattern matching on a list
+			myList match {
+				case firstTruck::restOfTheTrucks => <li>{firstTruck.name}</li>
+				case _ => <li>no trucks sorry!</li>
+			}
+
+  		}
   		</body>
   	</html>
   }
