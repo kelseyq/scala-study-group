ef23d47fd5a0400ec806ee02665e67d9c46a84ef
Kelsey <kelseyq@gmail.com>
2013-04-18 00:03:36 -0700

    add local jetty runner & sbt-idea plugin

--- project/build.scala
+++ project/build.scala
@@ -26,6 +26,7 @@ object ScalaStudyGroupBuild extends Build {
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "compile",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
       ),
